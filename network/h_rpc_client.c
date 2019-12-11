a8023e7877f1de490ef5b65feb3a51dc50037863
suzuki05 <>
2019-12-12 00:18:46 +0900

    client側をbufをダンプ出力するように修正

--- network/h_rpc_client.c
+++ network/h_rpc_client.c
@@ -8,19 +8,8 @@
 #define RLS_VER   ((u_long)1)
 #define RLS_PROC  ((u_long)1)
 
-int call_rls(char *host, char *buf, char **ans){
-  int callrpc(char *, unsigned long, unsigned long, unsigned long, xdrproc_t, char *, xdrproc_t, char *);
-  bool_t xdr_rls();
-  enum clnt_stat clnt_stat;
-
-  clnt_stat = callrpc(host, RLS_PROG, RLS_VER, RLS_PROC, (xdrproc_t)xdr_wrapstring, (char *)&buf, (xdrproc_t)xdr_wrapstring, (char *)ans);
-  if(clnt_stat != 0){
-    clnt_perrno(clnt_stat);
-  }
-  return(0);
-}
-
 int main(int argc, char *argv[]){
+  int call_rls(char *, char *, char **);
   char buf[BUF_SIZE];
   char *ans = NULL;
 
@@ -30,11 +19,30 @@ int main(int argc, char *argv[]){
   }
   strcpy(buf, argv[2]);
   call_rls(argv[1], buf, &ans);
+  /* ansの二重解放でクラッシュしてしまうためコメントアウト
   if(ans != NULL){
     free(ans);
     ans = NULL;
   }else{
     printf("ans = NULL\n");
+  }
+  */
+  printf("Response:\n%s\n", buf);   //bufのダンプで出力させるが、それは意図通りなのか・・・？
+  return (0);
+}
+
+bool_t xdr_rls(XDR *xdrs, char *objp){
+  return(xdr_string(xdrs, &objp, BUF_SIZE));
+}
+
+int call_rls(char *host, char *buf, char **ans){
+  int callrpc(char *, unsigned long, unsigned long, unsigned long, xdrproc_t, char *, xdrproc_t, char *);
+  bool_t xdr_rls();
+  enum clnt_stat clnt_stat;
+
+  clnt_stat = callrpc(host, RLS_PROG, RLS_VER, RLS_PROC, (xdrproc_t)xdr_rls, (char *)buf, (xdrproc_t)xdr_rls, (char *)ans);
+  if(clnt_stat != 0){
+    clnt_perrno(clnt_stat);
   }
   return(0);
 }
