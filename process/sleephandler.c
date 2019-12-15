56d63341fd054b025fbf918891d00b01c6df4093
suzuki05 <>
2019-12-15 11:07:42 +0900

    コメントの追加

--- process/sleephandler.c
+++ process/sleephandler.c
@@ -3,6 +3,7 @@
 #include <signal.h>
 #include <unistd.h>
 
+/* 待ちフラグ */
 static int ringring = 0;
 
 static void sleephandler(int sig){
@@ -17,23 +18,28 @@ static void sleephandler(int sig){
 /* 1マイクロ秒(1us)           1000nsec */
 /* 1ナノ秒(1ns)                  1nsec */
 /***************************************/
-
 int MicroSleep(long sec, long usec){
   struct itimerval val, oval;
   sigset_t mask;
 
+  /* 待つ必要なし */
   if(sec == 0 && usec == 0){
     return(0);
   }
 
+  /* 実時間タイマの現在値をクリア */
   timerclear(&val.it_interval);
   timerclear(&val.it_value);
+
+  /* インターバルタイマの値をセット */
   if(setitimer(ITIMER_REAL, &val, &oval) < 0){
     return(sec);
   }
+
   fprintf(stderr, "\tbefore = %lu.%06d\n", oval.it_value.tv_sec, oval.it_value.tv_usec);
   fprintf(stderr, "\t    -%lu.%06lu\n", sec, usec);
 
+  /* 実時間タイマの現在値と指定値を比較し残り時間を計算しておく */
   val.it_value.tv_sec = sec;
   val.it_value.tv_usec = usec;
   if(timerisset(&oval.it_value)){
@@ -46,12 +52,14 @@ int MicroSleep(long sec, long usec){
     }
   }
 
+  /* シグナルハンドラ登録 */
   signal(SIGALRM, sleephandler);
-
   ringring = 0;
 
+  /* 実時間タイマに指定時間をセット */
   setitimer(ITIMER_REAL, &val, (struct itimerval *)NULL);
 
+  /* 待ち */
   sigfillset(&mask);
   sigdelset(&mask, SIGALRM);
   while(!ringring){
@@ -61,6 +69,7 @@ int MicroSleep(long sec, long usec){
 
   ringring = 0;
 
+  /* 元のタイマの残り時間をセット */
   setitimer(ITIMER_REAL, &oval, (struct itimerval *)NULL);
   fprintf(stderr, "\tset   =%lu.%06d\n", oval.it_value.tv_sec, oval.it_value.tv_usec);
   return(0);
