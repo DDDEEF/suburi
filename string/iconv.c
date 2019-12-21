82959c4067ce3163265dabaa019333e5a0a76f7b
suzuki05 <>
2019-12-21 21:10:46 +0900

    UTF-8でiconvするように修正

--- string/iconv.c
+++ string/iconv.c
@@ -3,7 +3,7 @@
 #include <iconv.h>
 
 int main(){
-  char eucstr[512] = "あいうえおabcde";
+  char utf8str[512] = "あいうえおabcde";
   char sjisstr[512];
   iconv_t cd;
   size_t sleft;
@@ -12,13 +12,14 @@ int main(){
   char *sp;
   char *dp;
 
-  if((cd = iconv_open("SHIFT-JIS", "EUC-JP")) == (iconv_t*)-1){
+  /* 固有のUTF-8文字列をSJISに変換 */
+  if((cd = iconv_open("SHIFT-JIS", "UTF-8")) == (iconv_t*)-1){
     perror("iconv_open");
     return(-1);
   }
-  sleft = strlen(eucstr);
+  sleft = strlen(utf8str);
   dleft = sizeof(sjisstr) - 1;
-  for(sp = eucstr, dp = sjisstr; *sp != '\0'; ){
+  for(sp = utf8str, dp = sjisstr; *sp != '\0'; ){
     status = iconv(cd, &sp, &sleft, &dp, &dleft);
     fprintf(stderr, "[%zu]\n", status);
   }
@@ -26,19 +27,20 @@ int main(){
   *dp = '\0';
   printf("sjisstr = %s\n", sjisstr);
 
-  if((cd = iconv_open("EUC-JP", "SHIFT-JIS")) == (iconv_t*)-1){
+  /* UTF-8に戻す */
+  if((cd = iconv_open("UTF-8", "SHIFT-JIS")) == (iconv_t*)-1){
     perror("iconv_open");
     return(-1);
   }
   sleft = strlen(sjisstr);
-  dleft = sizeof(eucstr) - 1;
-  for(sp = sjisstr, dp = eucstr; *sp != '\0'; ){
+  dleft = sizeof(utf8str) - 1;
+  for(sp = sjisstr, dp = utf8str; *sp != '\0'; ){
     status = iconv(cd, &sp, &sleft, &dp, &dleft);
     fprintf(stderr, "<%zu>\n", status);
   }
   iconv_close(cd);
   *dp = '\0';
-  printf("eucstr = %s\n", eucstr);
+  printf("utf8str = %s\n", utf8str);
 
   return(0);
 }
