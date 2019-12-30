d83bdc12484d5eb2ff268c4ce739ea6ffa0b916b
suzuki05 <>
2019-12-30 10:06:58 +0900

    文字コード変換関連共有ライブラリ

---
+++ network/datasync/jconv.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <string.h>
+#include <iconv.h>
+#include "log.h"
+#include "jconv.h"
+
+/* EUC-JP を SJIS 変換関数 */
+int ujtosj(char *to, int tosize, char *from){
+  iconv_t cd;
+  size_t sleft;
+  size_t dleft;
+  size_t status;
+  char *sp;
+  char *dp;
+
+  if((cd = iconv_open("SJIS", "eucJP")) == (iconv_t*)-1){
+    SyslogPerror(LOG_ERR, "iconv_open");
+    return(-1);
+  }
+
+  sleft = strlen(from);
+  dleft = tosize - 1;
+  for(sp = from, dp = to; *sp != '\0'; ){
+    if((status = iconv(cd, &sp, &sleft, &dp, &dleft)) == (size_t)(-1)){
+      SyslogPerror(LOG_ERR, "iconv");
+      iconv_close(cd);
+      return(-1);
+    }
+  }
+  iconv_close(cd);
+  *dp = '\0';
+
+  return(0);
+}
+
+/* SJIS を EUC-JP 変換関数 */
+int sjtouj(char *to, int tosize, char *from){
+  iconv_t cd;
+  size_t sleft;
+  size_t dleft;
+  size_t status;
+  char *sp;
+  char *dp;
+
+  if((cd = iconv_open("eucJP", "SJIS")) == (iconv_t*)-1){
+    SyslogPerror(LOG_ERR, "iconv_open");
+    return(-1);
+  }
+
+  sleft = strlen(from);
+  dleft = tosize - 1;
+  for(sp = from, dp = to; *sp != '\0'; ){
+    if((status = iconv(cd, &sp, &sleft, &dp, &dleft)) == (size_t)(-1)){
+      SyslogPerror(LOG_ERR, "iconv");
+      iconv_close(cd);
+      return(-1);
+    }
+  }
+  iconv_close(fd);
+  *dp = '\0';
+
+  return(0);
+}
---
+++ network/datasync/jconv.h
@@ -0,0 +1,7 @@
+#ifndef _JIS_H_
+#define _JIS_H_
+
+int ujtosj(char *to, int tosize, char *from);
+int sjtouj(char *to, int tosize, char *from);
+
+#endif
