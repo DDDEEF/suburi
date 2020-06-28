fa981c321a07f97ee920d0368abca61d7a4760b3
suzuki05 <firefox3991@gmail.com>
2020-06-28 11:58:41 +0000

    アーカイブしたフォルダ・ファイルの削除

--- compression/chapter2/list2-16.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIST_H
-#define LIST_H
-
-extern void initCoordinateData(ImageData *, Coordinate *);
-extern int getNextCoordinate(ImageData *, Coordinate*);
-
-#endif /* LIST_H */
-
--- compression/chapter2/list2-17.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIST_H
-#define LIST_H
-
-extern void initCoordinateData(ImageData *, Coordinate *, int );
-extern int getNextCoordinate(ImageData *, Coordinate *);
-
-#endif /* LIST_H */
