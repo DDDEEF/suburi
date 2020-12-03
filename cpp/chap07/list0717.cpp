528a6a1b4abfd73c143a34c8649e58640047adc4
suzuki05 <firefox3991@gmail.com>
2020-12-03 02:28:30 +0000

    ポインタ

---
+++ cpp/chap07/list0717.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int *x = new int(5);
+  cout << "*x = " << *x << '\n';
+  delete x;
+}
---
+++ cpp/chap07/list07c01.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  cout << "sizeof(int) = " << sizeof(int) << '\n';
+  cout << "sizeof(int*) = " << sizeof(int*) << '\n';
+}
