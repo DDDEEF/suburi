bbf04044d022c0e86e115c46a0fe8a20ed9c23dc
suzuki05 <firefox3991@gmail.com>
2020-12-05 14:58:25 +0000

    冠すテンプレート

---
+++ cpp/chap09/list0903.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+template <class Type>
+Type maxof(Type a, Type b){
+  return a > b ? a : b;
+}
+
+int main(){
+  int a, b;
+  double x;
+
+  cout << "整数a:"; cin >> a;
+  cout << "整数b:"; cin >> b;
+  cout << "整数c:"; cin >> x;
+  
+  cout << "aとbで大きいのは" << maxof(a, b) << "です。\n";
+  cout << "aとxで大きいのは" << maxof<double>(a, x) << "です。\n";
+
+  cout << "aとbで大きいのは" << maxof(a, b) << "です。\n";
+  cout << "aとxで大きいのは" << maxof<double>(a, x) << "です。\n";
+}
