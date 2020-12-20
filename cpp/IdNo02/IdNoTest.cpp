5e3def37b6dd4bc1e04e6f1f86ccb9c16dbb02f4
suzuki05 <firefox3991@gmail.com>
2020-12-20 14:42:13 +0000

    静的データメンバ 静的メンバ関数

---
+++ cpp/IdNo02/IdNoTest.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "IdNo.h"
+
+using namespace std;
+
+int main(){
+  IdNo a;
+  IdNo b;
+
+  cout << "aの識別番号:" << a.id() << '\n';
+  cout << "bの識別番号:" << b.id() << '\n';
+  cout << "現在までに与えた識別番号の最大値:" << IdNo::get_max_id() << '\n';
+}
