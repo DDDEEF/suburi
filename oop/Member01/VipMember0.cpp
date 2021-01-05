091f21ddb852eed2320adb235766b4248437d48d
suzuki05 <firefox3991@gmail.com>
2021-01-05 13:41:47 +0000

    会員クラスと優待会員クラス

---
+++ oop/Member01/VipMember0.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <iostream>
+#include "VipMember0.h"
+
+using namespace std;
+
+VipMember0::VipMember0(const string& name, int no, double w, const string& prv) : full_name(name), number(no)
+{
+  set_weight(w);
+  set_privilege(prv);
+}
+
+void VipMember0::print() const
+{
+  cout << "No." << no() << ":" << name() << "(" << get_weight() << "kg)"
+       << "特典=" << privilege << '\n';
+}
