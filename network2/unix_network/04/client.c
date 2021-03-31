64ce12166f596a26f6eddc4a941f645d194a439f
lef2 <firefox3991@gmail.com>
2021-04-01 01:17:18 +0900

    1対1非同期通信

---
+++ network2/unix_network/04/client.c
@@ -0,0 +1,24 @@
+#include "session.h"
+#include "tag.h"
+
+main()
+{
+  int soc;
+  char hostname[HOSTNAME_LENGTH];
+
+  /* サーバのホスト名の入力 */
+  printf("input server's hostname: ");
+  fgets(hostname, HOSTNAME_LENGTH, stdin);
+  chop_newline(hostname, HOSTNAME_LENGTH);
+
+  /* 接続まで */
+  if((soc = setup_client(hostname, PORT)) == -1){
+    exit(1);
+  }
+
+  /* セッションモジュールの初期化 */
+  session_init(soc, 'x', 10, 10, 'o', 1, 1);
+
+  /* セッションのループ */
+  session_loop();
+}
